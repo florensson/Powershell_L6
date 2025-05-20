5a1eb1e4c06fefa19c84f9f94240373932e6fcb3
Fredrik <72890914+florensson@users.noreply.github.com>
2025-05-20 10:58:49 +0200

    Update getApiKey.ps1

--- getApiKey.ps1
+++ getApiKey.ps1
@@ -1,7 +1,7 @@
 function Get-Weather {
     param([string]$city)
 
-    $key = "b296d76c7c3946698de153807251905"
+    $key = ""
     $url = "http://api.weatherapi.com/v1/current.json?key=$key&q=$city"
 
     $response = Invoke-RestMethod -Uri $url
