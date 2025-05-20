da3eff69a7ea7783131af2c7bbf2c5b05580fd1c
Fredrik <72890914+florensson@users.noreply.github.com>
2025-05-20 10:59:24 +0200

    Update psscriptteminal.ps1

--- psscriptteminal.ps1
+++ psscriptteminal.ps1
@@ -1,7 +1,7 @@
 $response | Get-Member
 $response.PSObject.Properties.Name
 
-$token = "76464c5b5a3247459cfbce3cefa96e22"
+$token = ""
 $url = "https://newsapi.org/v2/top-headlines?country=us&category=technology"
 $headers = @{ "X-Api-Key" = $token }
 
